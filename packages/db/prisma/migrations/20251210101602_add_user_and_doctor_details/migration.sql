698f05eec04594f5b3177cb5b072683fcfd0a50d
Sujit Bista <sujitbistaprogrammer@gmail.com>
2025-12-10 11:35:49 +0545

    chore: add migration to add user and doctor detail fields
    - Add firstName, lastName, phoneNumber to users table
    - Add licenseNumber, address, city, state, zipCode, yearsOfExperience, education, profilePictureUrl to doctors table
    - This migration must run before make_user_fields_required migration

---
+++ packages/db/prisma/migrations/20251210101602_add_user_and_doctor_details/migration.sql
@@ -0,0 +1,14 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN "firstName" TEXT,
+ADD COLUMN "lastName" TEXT,
+ADD COLUMN "phoneNumber" TEXT;
+
+-- AlterTable
+ALTER TABLE "doctors" ADD COLUMN "licenseNumber" TEXT,
+ADD COLUMN "address" TEXT,
+ADD COLUMN "city" TEXT,
+ADD COLUMN "state" TEXT,
+ADD COLUMN "zipCode" TEXT,
+ADD COLUMN "yearsOfExperience" INTEGER,
+ADD COLUMN "education" TEXT,
+ADD COLUMN "profilePictureUrl" TEXT;
